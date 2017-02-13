6fa4af0808021f7cd9574dd8827a7ceead24d980
Andrea Casino <andrea.casino.2@gmail.com>
2017-02-13 18:22:43 +0000

    first commit

---
+++ build/3rdparty/libigl/external/nanogui/CMakeFiles/bin2c.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/andreacasino/Documents/Work/Aquisition/Coursework1/compM080-compGV18/IGLFramework/3rdparty/libigl/external/nanogui/resources/bin2c.c" "/Users/andreacasino/Documents/Work/Aquisition/Coursework1/compM080-compGV18/IGLFramework/build/3rdparty/libigl/external/nanogui/CMakeFiles/bin2c.dir/resources/bin2c.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CORO_SJLJ"
+  "GLAD_GLAPI_EXPORT"
+  "IGL_VIEWER_WITH_NANOGUI"
+  "NANOGUI_SHARED"
+  "NVG_SHARED"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../3rdparty/libigl/include"
+  "../3rdparty/libigl/external/nanogui/ext/eigen"
+  "../3rdparty/libigl/external/nanogui/ext/glfw/include"
+  "../3rdparty/libigl/external/nanogui/ext/nanovg/src"
+  "../3rdparty/libigl/external/nanogui/include"
+  "3rdparty/libigl/external/nanogui"
+  "../3rdparty/libigl/external/nanogui/ext/coro"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
