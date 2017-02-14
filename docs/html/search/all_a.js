7445c957a84cf103420c29c1bf25943c3f9eb1df
Andrea CASINO <acasino@cs.ucl.ac.uk>
2017-02-14 19:45:24 +0000

    Removed garbage

--- docs/html/search/all_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['recalcnormals',['recalcNormals',['../namespaceacq.html#ab45798d912ef5f55901bb3feb07a57df',1,'acq']]]
-];
--- docs/html/search/files_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['cloudmanager_2ecpp',['cloudManager.cpp',['../cloud_manager_8cpp.html',1,'']]],
-  ['cloudmanager_2eh',['cloudManager.h',['../cloud_manager_8h.html',1,'']]],
-  ['cloudmanager_2ehpp',['cloudManager.hpp',['../cloud_manager_8hpp.html',1,'']]]
-];
